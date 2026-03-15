33a86d2da7a0792f0de70fd04c8b5008de6a0e62
Dhruv Sood <d2sood@ucsd.edu>
2026-03-15 14:28:48 -0700

    Initial Commit

---
+++ FairwayIQ/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FairwayIQ
+//
+//  Created by Dhruv Sood on 3/15/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
